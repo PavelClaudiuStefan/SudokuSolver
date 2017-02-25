1ceabca9e50819ad1c61a08f8d0895031e0acc66
Pavel Claudiu Stefan <pavelclaudiu96@gmail.com>
2017-02-25 21:57:27 +0200

    First commit of a fully Sudoku solving program
    Solves hard and easier.

--- Main.py
+++ Main.py
@@ -1,8 +1,11 @@
+# http://www.extremesudoku.info/sudoku.html evil sudoku
+
 from SudokuSquare import SudokuSquare
 
-sudoku_1 = SudokuSquare("sudoku1.in")
-sudoku_1.print()
-while sudoku_1.completed() == False:
-    sudoku_1.solve()
-    sudoku_1.print()
+# sudoku = SudokuSquare("sudoku1.in")   # (Solved) Made by me
+sudoku = SudokuSquare("sudoku2.in")   # (Solved) Hard Sudoku
+# sudoku = SudokuSquare("sudoku3.in")   # Extreme Sudoku (Not solvable yet)
+# sudoku = SudokuSquare("sudoku4.in")   # Evil Sudoku (Not solvable yet
 
+sudoku.print()
+sudoku.solve()
--- sudokuSquare.py
+++ sudokuSquare.py
@@ -21,85 +21,117 @@ class SudokuSquare:
             print()
         print('###################\n')
 
-    # Checks if there exists only one number that fits in a certain slot
-    # Runs only once
+    # Solves sudoku aquare
     def solve(self):
-        # Seeking for "definites"
+        while not self.completed():
+            self.solve_once()
+            self.print()
+
+    # Checks if there exists only one number that fits in a certain slot
+    def solve_once(self):
+        # Empty spots will have a list with all the possible solutions
         for i in range(9):
             for j in range(9):
                 if self.sudoku_square[i][j] == 0:
-                    self.sudoku_square[i][j] = self.sure_value(i, j)
+                    self.sudoku_square[i][j] = self.value(i, j)
 
-    # Returns a number that is certainly the only number that can fit in that slot, else 0
-    def sure_value(self, i, j):
-        # return x if x != 0 in square
-        numar = self.square_value(i, j)
+    def value(self, i, j):
+        # Check to see if there is a value that is found only here and not in any other list from the square
+        numar = self.check_in_square(i, j)
         if numar != 0:
             return numar
-        # return x if x != 0 in line
-        numar = self.line_value(i)
+        # Check to see if there is a value that is found only here and not in any other list from the line
+        numar = self.check_in_line(i, j)
         if numar != 0:
             return numar
-        # return x if x != 0 in column
-        numar = self.column_value(j)
+        # Check to see if there is a value that is found only here and not in any other list from the column
+        numar = self.check_in_column(i, j)
         if numar != 0:
             return numar
-        # return 0
         return 0
 
-    # Seek "definite" in a square
-    def square_value(self, i, j):
-        empty_slot = [0] * 10
-        s = 0
-        x = 0
-        i_patrat = int(i / 3) * 3
-        j_patrat = int(j / 3) * 3
-        for ii in range(i_patrat, i_patrat + 3):
-            for jj in range(j_patrat, j_patrat + 3):
-                if self.sudoku_square[ii][jj] != 0:
-                    empty_slot[self.sudoku_square[ii][jj]] = 1
-        for iii in range(1, 10):
-            s += empty_slot[iii]
-            if empty_slot[iii] == 0:
-                x = iii
-        if s == 8:
-            return x
+    def check_in_square(self, i, j):
+        lista_principala = []
+        lista_secundara = []
+        for x in range(1, 10):
+            if not self.in_square(x, i, j) and not self.in_line(x, i) and not self.in_column(x, j):
+                # Appends a possible solution
+                lista_principala.append(x)
+        i_s = int(i / 3) * 3
+        j_s = int(j / 3) * 3
+        for ii in range(i_s, i_s + 3):
+            for jj in range(j_s, j_s + 3):
+                if self.sudoku_square[ii][jj] == 0 and (ii != i or jj != j):
+                    for x in range(1, 10):
+                        if not self.in_square(x, ii, jj) and not self.in_line(x, ii) and not self.in_column(x, jj):
+                            # Appends a possible solution
+                            lista_secundara.append(x)
+        if len(lista_principala) == 1:
+            return lista_principala[0]
         else:
-            return 0
+            for x in lista_principala:
+                if x not in lista_secundara:
+                    return x
+        return 0
 
-    # Seek "definite" in a line
-    def line_value(self, i):
-        empty_slot = [0] * 10
-        s = 0
-        x = 0
+    def check_in_line(self, i, j):
+        lista_principala = []
+        lista_secundara = []
+        for x in range(1, 10):
+            if not self.in_square(x, i, j) and not self.in_line(x, i) and not self.in_column(x, j):
+                lista_principala.append(x)
         for ii in range(9):
-            if self.sudoku_square[i][ii] != 0:
-                empty_slot[self.sudoku_square[i][ii]] = 1
-        for ii in range(1, 10):
-            s += empty_slot[ii]
-            if empty_slot[ii] == 0:
-                x = ii
-        if s == 8:
-            return x
+            if self.sudoku_square[i][ii] == 0 and ii != j:
+                for x in range(1, 10):
+                    if not self.in_square(x, i, ii) and not self.in_line(x, i) and not self.in_column(x, ii):
+                        lista_secundara.append(x)
+        if len(lista_principala) == 1:
+            return lista_principala[0]
         else:
-            return 0
+            for x in lista_principala:
+                if x not in lista_secundara:
+                    return x
+        return 0
 
-    # Seek "definite" in a column
-    def column_value(self, j):
-        empty_slot = [0] * 10
-        s = 0
-        x = 0
+    def check_in_column(self, i, j):
+        lista_principala = []
+        lista_secundara = []
+        for x in range(1, 10):
+            if not self.in_square(x, i, j) and not self.in_line(x, i) and not self.in_column(x, j):
+                lista_principala.append(x)
         for jj in range(9):
-            if self.sudoku_square[jj][j] != 0:
-                empty_slot[self.sudoku_square[jj][j]] = 1
-        for jj in range(1, 10):
-            s += empty_slot[jj]
-            if empty_slot[jj] == 0:
-                x = jj
-        if s == 8:
-            return x
+            if self.sudoku_square[jj][j] == 0 and jj != i:
+                for x in range(1, 10):
+                    if not self.in_square(x, jj, j) and not self.in_line(x, jj) and not self.in_column(x, j):
+                        lista_secundara.append(x)
+        if len(lista_principala) == 1:
+            return lista_principala[0]
         else:
-            return 0
+            for x in lista_principala:
+                if x not in lista_secundara:
+                    return x
+        return 0
+
+    def in_square(self, nr, i, j):
+        i_s = int(i / 3) * 3
+        j_s = int(j / 3) * 3
+        for ii in range(i_s, i_s + 3):
+            for jj in range(j_s, j_s + 3):
+                if nr == self.sudoku_square[ii][jj]:
+                    return True
+        return False
+
+    def in_line(self, nr, i):
+        for ii in range(9):
+            if nr == self.sudoku_square[i][ii]:
+                return True
+        return False
+
+    def in_column(self, nr, j):
+        for jj in range(9):
+            if nr == self.sudoku_square[jj][j]:
+                return True
+        return False
 
     # Function returns true if square is completely filled
     def completed(self):
